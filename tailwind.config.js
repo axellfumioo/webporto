85a5b394478580c498930604edc118fed2ee46be
axellfumioo <axelazharputra@gmail.com>
2025-01-25 22:19:04 +0700

    Migrating to V2

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-// Run Command: npx tailwindcss -i ./assets/tailwind-input.css -o ./assets/style.css --watch
-
-module.exports = {
-    content: ["./index.html"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-}
