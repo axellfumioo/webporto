6af4a2c4c4b8860cfb9d2091f12cdc94c01cb5fa
axellfumioo <axelazharputra@gmail.com>
2025-03-15 20:21:01 +0700

    jos

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
-module.exports = {
   rules: {
     "react/no-unescaped-entities": "off",
-  },
+  }
 };
 export default nextConfig;
