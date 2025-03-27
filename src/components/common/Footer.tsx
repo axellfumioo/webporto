935b85457fd58c2bd69a0166ec5ec9e071e7e17a
axellfumioo <axelazharputra@gmail.com>
2025-03-27 22:00:22 +0700

    update footer copyright text to use anchor tag for improved accessibility

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <div className='mt-14 mb-4'>
       <hr className="h-px mt-8 mb-4 bg-[#2E2E2E] border-0" />
       <div className='flex justify-center mb-2'>
-        <h1 className='text-[#A7A7A7]'>Copyright &copy; 2025 Axel Azhar Putra Ananca. All Rights Reserved.</h1>
+        <a className='text-[#A7A7A7]'>Copyright &copy; 2025 Axel Azhar Putra Ananca. All Rights Reserved.</a>
       </div>
     </div>
   );
